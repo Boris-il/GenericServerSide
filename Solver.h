4a1677946ebedc33c11747b83d590bd420b25ebe
YuvalMachlev <yuvalm100@gmail.com>
2020-01-09 14:12:58 +0200

    lets start

---
+++ Solver.h
@@ -0,0 +1,13 @@
+//
+// Created by yuval on 09/01/2020.
+//
+
+#ifndef GENERICSERVERSIDE_CMAKE_BUILD_DEBUG_SOLVER_H_
+#define GENERICSERVERSIDE_CMAKE_BUILD_DEBUG_SOLVER_H_
+
+class Solver{
+ public:
+  //todo solve Problem and return solution as classes ?
+  virtual Solution solve(Problem p);
+};
+#endif //GENERICSERVERSIDE_CMAKE_BUILD_DEBUG_SOLVER_H_
