2bcc7c2a8c868117bd6f55e7345a36d05d342e26
YuvalMachlev <yuvalm100@gmail.com>
2020-01-16 15:30:52 +0200

    b4 algorithm implementation

--- Searcher.h
+++ Searcher.h
@@ -6,11 +6,42 @@
 #define GENERICSERVERSIDE__SEARCHER_H_
 
 #include "Searchable.h"
+#include <queue>
 
 template<class S>
-class Searcher {
-  virtual S search(Searchable<S>) =0;
+class ISearcher {
+  virtual S search(Searchable<S> searchable) = 0;
+  virtual int getNumberOfNodesEvaluated() = 0;
 
+};
+
+template<class S>
+class searcher : public ISearcher<S> {
+  priority_queue<State<S>> openList;
+  int evaluatedNodes;
+ public:
+  searcher() {
+    openList = new priority_queue<State<S>>;
+    evaluatedNodes = 0;
+  }
+  int getOpenListSize() {
+    return openList.size();
+  }
+
+  virtual S search(Searchable<S> searchable) override = 0;
+
+  int getNumberOfNodesEvaluated() override {
+    return evaluatedNodes;
+  }
+ protected:
+  State<S> popOpenList() {
+    evaluatedNodes++;
+    return openList.pop();
+  }
+};
+
+template<class S>
+class BFS : public searcher<S> {
 
 };
 
